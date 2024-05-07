9fcc62df2c6392dfe2492e0fc8ce0475da16ec00
KJHJason <kuanjunhaojason@gmail.com>
2024-05-08 00:51:48 +0800

    added dummy placeholder

--- frontend/src/lib/settings/Sessions.svelte
+++ frontend/src/lib/settings/Sessions.svelte
@@ -4,15 +4,15 @@
 
 <div class="grid grid-cols-1">
     <div>
-        <Session title="Fantia" elId="fantia-session" website="fantia" placeholder="Fantia Session Cookie" />
+        <Session title="Fantia" elId="fantia-session" website="fantia" placeholder="96854bb23e733aa0d44211936a3e1f8a8185f6bdb50e04047a845824ade3c2d0" />
     </div>
     <div>
-        <Session title="Pixiv Fanbox" elId="pixiv-fanbox-session" website="fanbox" placeholder="Pixiv Fanbox Session Cookie" />
+        <Session title="Pixiv Fanbox" elId="pixiv-fanbox-session" website="fanbox" placeholder="12312345_rAkUgAK1cH4nBeSTwA1FuSEmmmAZsEgG" />
     </div>
     <div>
-        <Session title="Pixiv Web" elId="pixiv-session" website="pixiv" placeholder="Pixiv Session Cookie" />
+        <Session title="Pixiv Web" elId="pixiv-session" website="pixiv" placeholder="54321234_rAkUgAK1cH4nBeSTwA1FuSEmmmAZSeGg" />
     </div>
     <div>
-        <Session title="Kemono" elId="kemono-session" website="kemono" placeholder="Kemono Session Cookie" />
+        <Session title="Kemono" elId="kemono-session" website="kemono" placeholder="eyJfcGVybWFuZW50Ijp0cnVlLCJhY2NvdW50X2lkIjoicmFrdWdha2ktY2hhbiJ9.eJGV..." />
     </div>
 </div>
