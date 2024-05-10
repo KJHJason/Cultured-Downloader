dd3df30bf23d37378342c61f0ec37f498df251d3
KJHJason <kuanjunhaojason@gmail.com>
2024-05-10 18:01:37 +0800

    add Loading.svelte

---
+++ frontend/src/lib/Loading.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { onDestroy } from "svelte";
+    import { swal } from "../scripts/constants";
+
+    swal.fire({
+        title: "",
+        text: "Please wait for the program to load...",
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false,
+        showConfirmButton: false,
+        scrollbarPadding: false,
+        willOpen: () => {
+            swal.showLoading();
+        }
+    });
+
+    onDestroy(() => {
+        swal.close();
+    });
+</script>
